c9e918e921b283fc6f53b14ad105c75f6f6404be
makovkastar <makovkastar@gmail.com>
2014-11-03 20:21:49 +0200

    Add more operator logos

--- app/src/main/java/com/melnykov/callmeback/Operators.java
+++ app/src/main/java/com/melnykov/callmeback/Operators.java
@@ -14,14 +14,14 @@ public class Operators {
         new Operator(1, R.string.mts_ua, R.drawable.mts, Country.UKRAINE, "*104*%s#"),
         new Operator(2, R.string.beeline_ua, R.drawable.beeline, Country.UKRAINE, "*130*38%s#"),
         new Operator(3, R.string.djuice, R.drawable.djuice, Country.UKRAINE, "*130*38%s#"),
-        new Operator(4, R.string.life_ua, R.drawable.life_ua, Country.UKRAINE, "*123*3*38%s#"),
+        new Operator(4, R.string.life_ua, R.drawable.life, Country.UKRAINE, "*123*3*38%s#"),
         new Operator(5, R.string.utel, R.drawable.utel, Country.UKRAINE, "*110*02*%s#"),
         // Russia
         new Operator(100, R.string.mts_ru, R.drawable.mts, Country.RUSSIA, "*110*8%s#"),
         new Operator(101, R.string.beeline_ru, R.drawable.beeline, Country.RUSSIA, "*144*%s#"),
         new Operator(102, R.string.megafon, R.drawable.megafon, Country.RUSSIA, "*144*8%s#"),
         new Operator(103, R.string.rostelecom, R.drawable.rostelecom, Country.RUSSIA, "*123*8%s#"),
-        new Operator(104, R.string.tele2_ru, R.drawable.tele2_ru, Country.RUSSIA,  "*118*8%s#"),
+        new Operator(104, R.string.tele2_ru, R.drawable.tele2, Country.RUSSIA,  "*118*8%s#"),
         new Operator(105, R.string.motiv, R.drawable.motiv, Country.RUSSIA, "*105*8%s#"),
         new Operator(106, R.string.smarts, R.drawable.smarts, Country.RUSSIA, "*134*%s#"),
         new Operator(107, R.string.nss, R.drawable.nss, Country.RUSSIA, "*135*%s#"),
@@ -30,17 +30,17 @@ public class Operators {
         new Operator(110, R.string.etk, R.drawable.etk, Country.RUSSIA, "*102*50*%s#"),
         new Operator(111, R.string.justtotalk, R.drawable.justtotalk, Country.RUSSIA, "*168*%s#"),
         // Belarus
-        new Operator(200, R.string.velcom, 0, Country.BELARUS, "*131*%s#"),
-        new Operator(201, R.string.mts_by, 0, Country.BELARUS, "*120*%s#"),
-        new Operator(202, R.string.life_by, 0, Country.BELARUS,  "*120*2*%s#"),
+        new Operator(200, R.string.velcom, R.drawable.velcom, Country.BELARUS, "*131*%s#"),
+        new Operator(201, R.string.mts_by, R.drawable.mts, Country.BELARUS, "*120*%s#"),
+        new Operator(202, R.string.life_by, R.drawable.life, Country.BELARUS,  "*120*2*%s#"),
         // Moldova
-        new Operator(300, R.string.idc, 0, Country.MOLDOVA, "887#%s"),
-        new Operator(301, R.string.mold_cell, 0, Country.MOLDOVA, "*111*7%s#"),
+        new Operator(300, R.string.idc, R.drawable.idc, Country.MOLDOVA, "887#%s"),
+        new Operator(301, R.string.mold_cell, R.drawable.mold_cell, Country.MOLDOVA, "*111*7%s#"),
         // Kazakhstan
-        new Operator(400, R.string.kcell, 0, Country.KAZAKHSTAN, "*130*8%s#"),
-        new Operator(401, R.string.activ_kz, 0, Country.KAZAKHSTAN, "*130*8%s#"),
-        new Operator(402, R.string.beeline_kz, 0, Country.KAZAKHSTAN, "*144*8%s#"),
-        new Operator(403, R.string.tele2_kz, 0, Country.KAZAKHSTAN, "*144*8%s#")
+        new Operator(400, R.string.kcell, R.drawable.kcell, Country.KAZAKHSTAN, "*130*8%s#"),
+        new Operator(401, R.string.activ_kz, R.drawable.activ, Country.KAZAKHSTAN, "*130*8%s#"),
+        new Operator(402, R.string.beeline_kz, R.drawable.beeline, Country.KAZAKHSTAN, "*144*8%s#"),
+        new Operator(403, R.string.tele2_kz, R.drawable.tele2, Country.KAZAKHSTAN, "*144*8%s#")
     );
 
     private Operators() {
